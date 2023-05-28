9c7a6d6c9405a82db31212ea15811881025cba1f
Sandip Patel <sandippatelec@users.noreply.github.com>
2023-05-28 16:39:10 +0530

    Update main.c

--- analytics/C/udataDatabase/src/main.c
+++ analytics/C/udataDatabase/src/main.c
@@ -6,6 +6,6 @@ int main()
 
 	printf("Hello CI Pipeline\n");
 	printf("Hello CI Pipeline Check\n");
-	printf("Pipeline Fail check on Error\n")
+	printf("Pipeline Fail check on Error\n");
 	return 0;
 }
